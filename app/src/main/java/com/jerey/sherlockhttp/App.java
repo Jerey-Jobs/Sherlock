dc821d4661a542dcfb04dcc75b88593567688f7b
Xiamin <610315802@qq.com>
2017-07-16 17:04:25 +0800

    增加使用demo

---
+++ app/src/main/java/com/jerey/sherlockhttp/App.java
@@ -0,0 +1,30 @@
+package com.jerey.sherlockhttp;
+
+import android.app.Application;
+
+import com.jerey.sherlockimageloader.ImageLoaderConfig.ImageLoaderConfig;
+import com.jerey.sherlockimageloader.SherlockImageLoader;
+import com.jerey.sherlockimageloader.cache.DoubleCache;
+
+/**
+ * @author Xiamin
+ * @date 2017/7/16
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SherlockImageLoader.init(this,
+                new ImageLoaderConfig.Builder()
+                        .setBitmapCache(new DoubleCache(this))
+                        .setFailedImage(R.mipmap.ic_launcher)
+                        .setLoadingImage(R.drawable.blog)
+                        .build());
+    }
+
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
